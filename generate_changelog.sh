5767427ce4b5fb30fb89b1a840ab1ba08e213247
Lee Yong Hyun <lyhlg0201@gmail.com>
2024-05-08 23:41:45 +0900

    feat: add change generator

---
+++ generate_changelog.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Ensure all tags are fetched
+git fetch --tags
+
+# Get the latest tag name
+latest_tag=$(git describe --tags --abbrev=0)
+
+# Fetch commits from latest tag to HEAD
+commits=$(git log $latest_tag..HEAD --pretty=format:"%s (%h)" --abbrev-commit)
+
+# Initialize associative array to hold categorized commits
+declare -A changelog_groups
+
+# Parse each commit line
+while IFS= read -r line; do
+    # Extract the commit type and description
+    type=$(echo "$line" | cut -d ':' -f 1)
+    message=$(echo "$line" | sed -e "s/$type: //" | sed -e "s/ ([a-f0-9]+)$//")
+    commit_hash=$(echo "$line" | grep -o '(\w\+)$' | sed 's/[()]//g')
+    link="https://github.com/lyhlg/lerna-pnpmZ/commit/$commit_hash"
+    formatted_message="- $message ([#$commit_hash]($link))"
+    changelog_groups["$type"]+="$formatted_message\n"
+done <<< "$commits"
+
+# Output the formatted changelog
+echo "CHANGELOG=''" >> $GITHUB_ENV
+for type in "${!changelog_groups[@]}"; do
+    echo "CHANGELOG+='# $type\n${changelog_groups[$type]}\n'" >> $GITHUB_ENV
+done
